3a72d18bbf652589f14f685af8476da81212d892
ValdsonSilva <valdsonmacedo15@gmail.com>
2026-04-06 23:22:02 -0300

    feat: add defaultTimeZone field to UpdateUserSchema for user updates

--- modules/auth/auth.schema.ts
+++ modules/auth/auth.schema.ts
@@ -17,4 +17,5 @@ export const UpdateUserSchema = z.object({
     name: z.string().min(1).optional(),
     phone: z.string().min(6).optional(),
     password: z.string().min(8).optional(),
+    defaultTimeZone: z.string().optional(),
 });
